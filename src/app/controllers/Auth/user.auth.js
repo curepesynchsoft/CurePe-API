675f32344928ec90dfa697566504612a69fc3836
Curepe <root@synchsoft.in>
2022-04-14 08:02:02 +0000

    required

--- src/app/controllers/Auth/user.auth.js
+++ src/app/controllers/Auth/user.auth.js
@@ -97,21 +97,14 @@ const verify_through_otp = async (request, reply) => {
 const update_User = async (request, reply) => {
   try {
     const update_document = {
-<<<<<<< HEAD
       full_name : request.body.full_name,
-=======
-      full_name : request.body.full_name, 
->>>>>>> 39b1ae252d1f024d6e8dd9212777a72f52ca9536
       gender :request.body.gender,
       dob: request.body.dob,
       health_id: request.body.health_id,
     };
-<<<<<<< HEAD
-=======
     if (request.body.full_name|request.body.gender|request.body.dob==""){
       return reply.code(400).send({error: "field required"});
     }
->>>>>>> 39b1ae252d1f024d6e8dd9212777a72f52ca9536
     const user = await user_model.update({ id: request.user.id }, update_document);
     if(user) {      
       const user_relative = await user_relative_model.findUnique(
@@ -125,10 +118,6 @@ const update_User = async (request, reply) => {
   }
 };
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 39b1ae252d1f024d6e8dd9212777a72f52ca9536
 // add relatives
 const add_members = async (request, reply) =>{
   let response = []
@@ -143,12 +132,9 @@ const add_members = async (request, reply) =>{
         dob: request.body.dob,
         relation: request.body.relation
       };
-<<<<<<< HEAD
-=======
       if (request.body.full_name|request.body.phone|request.body.dob|request.body.gender==""){
         return reply.code(400).send({error: "field required"});
       }
->>>>>>> 39b1ae252d1f024d6e8dd9212777a72f52ca9536
       response.add_members = await user_relative_model.create(new_member)
       if (response.add_members) {
         response.user = await createUser(request, reply);
